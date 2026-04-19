2c90b660747ce9a38d624b91140381f65ebc84d5
Srivarshini55 <srivarshinivel55@gmail.com>
2026-04-19 12:17:37 +0530

    fixed port for render

--- server/index.js
+++ server/index.js
@@ -173,6 +173,8 @@ app.get('/download/:filename', (req, res) => {
 
 // ================= SERVER =================
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
